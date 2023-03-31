ddaa3b6a8acdad3b6056c6adf61420aafb021a8a
expensivepens <65430555+expensivepens@users.noreply.github.com>
2023-03-31 13:59:11 +0200

    Make the number of lines generated by the generate button = to the input value

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 "use strict"
 
 let elPara = document.getElementById("el-paragraph");
+let numberInput = document.getElementById("number-input");
 
 function generate(){
-    let count = 0;
+    let count = 1;
     elPara.innerHTML = "";
     let intervalId = setInterval(() =>{
-        if (count < 10){
+        if (count <= numberInput.value){
             elPara.innerHTML = elPara.innerHTML + count + "<br>";
             count ++;
         } else {
             clearInterval(intervalId);
         }
-    } , 250);
+    } , 200);
 }
