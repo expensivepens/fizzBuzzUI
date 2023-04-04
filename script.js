ceef4b2caac83e47b28838ad7f03247bf0e5c244
expensivepens <65430555+expensivepens@users.noreply.github.com>
2023-04-04 15:22:43 +0200

    Fix a bug where clicking on generate restarted the generation without stopping the last one, so more than one were running at once

--- script.js
+++ script.js
@@ -2,17 +2,21 @@
 
 let elPara = document.getElementById("el-paragraph");
 let numberInput = document.getElementById("number-input");
+let intervalId;
 
 function generate(){
+    if (intervalId){
+        clearInterval(intervalId);
+    }
     let count = 1;
     elPara.innerHTML = "";
-    let intervalId = setInterval(() =>{
-        if (count <= numberInput.value){
-            elPara.innerHTML = elPara.innerHTML + count + " ";
-            count ++;
-        } else {
-            clearInterval(intervalId);
-        }
-    } , 150);
+        intervalId = setInterval(() =>{
+            if (count <= numberInput.value){
+                elPara.innerHTML = elPara.innerHTML + count + " ";
+                count ++;
+            } else {
+                clearInterval(intervalId);
+            }
+        } , 150);
 }
 // IF I CLICK ON GENERATE SEVERAL TIMES BEFORE IT ENDS IT GOES CRAZY
